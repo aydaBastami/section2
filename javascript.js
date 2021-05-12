6432c71e2d4d639ba87a14c28d1ecd823ea23311
aydaBastami <aydabstmi@gmail.com>
2021-05-12 06:04:07 -0700

    2

---
+++ javascript.js
@@ -0,0 +1,29 @@
+// let rolldie = Math.floor(Math.random() * 6 + 1)
+// console.log(rolldie);
+
+// function rolldie(nmside) {
+//     console.log(Math.floor(Math.random() * nmside + 1)); 
+// }
+
+// rolldie(6)
+
+// function rolldie(nmside) {
+//     if (nmside === undefined) {
+//         nmside = 6;
+//     }
+//    return Math.floor(Math.random() * nmside + 1);
+// }
+
+// rolldie(6)
+
+// function fullName(Fname = "Ayda", Lname = "Bastami") {
+//     console.log(`${Fname} ${Lname}`); 
+// }
+
+// fullName()
+
+// console.log([..."hello"])
+// // console.log(...hello);
+// console.log(..."hello");
+// console.log([...hello]);
+
